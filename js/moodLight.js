74b2b1b48b44a4013629eef52ab56d067d098c57
leekasong <leekasong@naver.com>
2017-07-13 12:37:28 +0900

    ㅌ글 스위치

---
+++ js/moodLight.js
@@ -0,0 +1,11 @@
+//draw moodlight
+
+
+  function mood_toggle(){
+    var toggle = document.getElementById('mood_toggle');
+    if(toggle.checked){
+      alert(1);
+    }else{
+      alert(0);
+    }
+  }
